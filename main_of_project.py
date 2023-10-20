5abb06f441485300f4e5033ca4cc11e90e430070
MrProVans <77919630+MrProVans@users.noreply.github.com>
2023-10-20 16:27:04 +0300

    Final_update

--- main_of_project.py
+++ main_of_project.py
@@ -1,10 +1,15 @@
 from functions_of_project import *
-from random import randint
+from random import choice
+
 print('Выберите место из предложенного списка, куда вы опоздали:')
 s = [f'{i+1}.{location[i]}\n' for i in range(len(location))]
 print(*s)
-num = int(input('Вводите: ')) -1
-rand = randint(1,len(excuse))
-print()
+
+num = input('Введите: ')
+while not(num.isdigit()) or not(1 <= int(num) <= 15):
+    num = input('Введите корректное значение: ')
+
+rand = choice(excuse)
+
 print('Отмазка: \n')
-print(f'Я не пришел {scloneniya[num]}, потому что {excuse[rand]}')
+print(f'Я не пришел {scloneniya[int(num) - 1]}, потому что {rand}')
