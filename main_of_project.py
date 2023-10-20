aa966389e272e1b7d01517c7658cb559ce659f71
smykovvd <vdsmykov@edu.hse.ru>
2023-10-20 15:59:50 +0300

    Lists were fixed  and was functional program for output was written by Smykov Viktor.

--- main_of_project.py
+++ main_of_project.py
@@ -1,1 +1,10 @@
-# Я не пришел в место "location", потому что "excuse"
+from functions_of_project import *
+from random import randint
+print('Выберите место из предложенного списка, куда вы опоздали:')
+s = [f'{i+1}.{location[i]}\n' for i in range(len(location))]
+print(*s)
+num = int(input('Вводите: ')) -1
+rand = randint(1,len(excuse))
+print()
+print('Отмазка: \n')
+print(f'Я не пришел {scloneniya[num]}, потому что {excuse[rand]}')
